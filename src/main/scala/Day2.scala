90c783cd46fec2efc4efcb682420e3b5aa4bd88b
osesantos <happycod3r@gmail.com>
2021-12-01 12:11:53 +0000

    setup day 2

---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,17 @@
+@main def main() =
+  val input = "day2".getInput.map(s => s.toInt)
+  day2_1(input)
+  day2_2(input)
+
+def day2_1(input: List[Int]): Unit =
+  // Day 2 Part 1
+  println(s"Day 2 - PART 1: ")
+
+def day2_2(input: List[Int]): Unit =
+  // Day 2 Part 1
+  println(s"Day 2 - PART 2: ")
+
+
+
+
+
