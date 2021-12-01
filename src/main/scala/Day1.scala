36fd6dba84bd95b3e3388b36354e834ac7bbd787
osesantos <happycod3r@gmail.com>
2021-12-01 12:07:36 +0000

    first day1 part2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,11 +1,23 @@
 @main def main() =
   val input = "day1".getInput.map(s => s.toInt)
   day1_1(input)
+  day1_2(input)
+
+extension (lst: List[Int]) {
+  def countIncrease: Int = lst.zipWithIndex.count((e, i) => i > 0 && e > lst(i - 1))
+}
 
 def day1_1(input: List[Int]): Unit =
-  println(s"Day 1 - PART 1: ${input.zipWithIndex.count((e, i) => i > 0 && e > input(i - 1))}")
+  println(s"Day 1 - PART 1: ${input.countIncrease}")
 
 def day1_2(input: List[Int]): Unit =
+  val newList = input.zipWithIndex.map((e, i) =>
+    if i < input.length - 2 then
+      e + input(i + 1) + input(i + 2)
+    else
+      0
+  )
+  println(s"Day 1 - PART 2: ${newList.countIncrease}")
 
 
 
--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -3,3 +3,4 @@ import scala.io.Source
 extension (str: String) {
   def getInput: List[String] = Source.fromFile(s"src/main/input/$str.txt").getLines().toList
 }
+
